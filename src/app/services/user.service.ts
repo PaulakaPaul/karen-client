ce98e9a3bf79562fac51e4f7e8c67c4711426270
oproiustefan <oproiustefan@yahoo.com>
2019-05-17 22:21:21 +0300

    Quick debugging

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Register } from '../models/register.model';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Login } from '../models/login.model';
 
@@ -12,11 +13,13 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  public signUp(model: Register) : Observable<string> {
-    return this.http.post<string>(`${environment.springApi}/user`, model);
+  public signUp(model: Register): Observable<string> {
+    return this.http.post<string>(`${environment.springApi}/user`, model)
+      .pipe(tap(console.log));
   }
 
-  public logIn(model: Login) : Observable<string> {
-    return this.http.post<string>(`${environment.springApi}/user/login`, model);
+  public logIn(model: Login): Observable<string> {
+    return this.http.post<string>(`${environment.springApi}/user/login`, model)
+      .pipe(tap(console.log));
   }
 }
